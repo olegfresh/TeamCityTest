84557231f86b02bd104cf33df6a55e25b40f4231
Oleg Prymak <oprymak@freshdirect.com>
2020-06-30 09:35:32 -0400

    initial commit.

---
+++ TeamCityApp/TeamCityApp/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using TeamCityApp.Models;
+
+namespace TeamCityApp.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
